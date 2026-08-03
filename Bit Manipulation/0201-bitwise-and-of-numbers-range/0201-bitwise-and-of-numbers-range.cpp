7ee5d170abf5d6507e5f8cdcdc4c69b57bf75d3f
Ravindra Yadav <140959657+ravindra-y@users.noreply.github.com>
2026-08-03 17:44:18 +0530

    Solved LeetCode #201: Bitwise AND of Numbers Range

--- Bit Manipulation/0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ Bit Manipulation/0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        int shiftCount = 0;
-
-        while (left != right) {
-            left = (left >> 1);
-            right = (right >> 1);
-            shiftCount++;
+        while (right > left) {
+            right = right & (right - 1);
         }
 
-        return left << shiftCount;
+        return right;
     }
 };
