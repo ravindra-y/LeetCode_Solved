0d8c49888ca876641619e98798c5e052c4477e71
Ravindra Yadav <140959657+ravindra-y@users.noreply.github.com>
2026-08-13 10:00:04 +0530

    Refactor groupAnagrams function for clarity

--- Hash Tables/0049-group-anagrams/0049-group-anagrams.cpp
+++ Hash Tables/0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> groups;
+        unordered_map<string, vector<string>> mp;
 
-        for (const string& s : strs) {
-            int freq[26] = {};
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
 
-            // Count frequency of each character
-            for (char c : s) {
-                freq[c - 'a']++;
-            }
-                
-            // Build key using characters and their counts (e.g., "a1e1t1")
-            string key;
-            for (int i = 0; i < 26; ++i) {
-                if (freq[i] > 0) {
-                    key += char('a' + i) + to_string(freq[i]);
-                }
-            }
-
-            groups[key].push_back(s);
+            mp[key].push_back(s);
         }
 
-        // Collect all grouped anagrams into result
-        vector<vector<string>> result;
-        for (auto& [key, group] : groups) {
-            result.push_back(move(group));
-        }    
+        vector<vector<string>> ans;
 
-        return result;
+        for (auto x : mp) {
+            ans.push_back(x.second);
+        }
+
+        return ans;
     }
 };
 
