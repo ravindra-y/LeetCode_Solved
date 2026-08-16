ede1ecf87cfe6202d494b94eb9442e90bd209b97
Ravindra Yadav <140959657+ravindra-y@users.noreply.github.com>
2026-08-16 15:50:28 +0530

    Solved LeetCode #242: Valid Anagram

---
+++ Hash Tables/0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        unordered_map<char, int> count;
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+
+        for (auto it : count) {
+            if (it.second != 0) return false;
+        }
+
+        return true;
+    }
+};
