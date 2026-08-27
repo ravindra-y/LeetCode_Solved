f35e4cbda173fa5f6481fe870ad7dc9c320eca62
Ravindra Yadav <140959657+ravindra-y@users.noreply.github.com>
2026-08-27 08:55:19 +0530

    Solved LeetCode #844: Backspace String Compare

---
+++ Two Pointers/0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        int skip_s = 0;
+        int skip_t = 0;
+
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skip_s++;
+                    i--;
+                } else if (skip_s > 0) {
+                    skip_s--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skip_t++;
+                    j--;
+                } else if (skip_t > 0) {
+                    skip_t--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            char first = i < 0 ? '$' : s[i];
+            char second = j < 0 ? '$' : t[j];
+
+            if (first != second) return false;
+
+            i--; j--;
+        }
+
+        return true;
+    }
+};
