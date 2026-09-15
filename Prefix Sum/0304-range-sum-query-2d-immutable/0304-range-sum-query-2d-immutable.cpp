4276a4a0ffc0952692bfb01a59603c9afdc4c3ca
Ravindra Yadav <140959657+ravindra-y@users.noreply.github.com>
2026-09-15 15:31:13 +0530

    Solved LeetCode #304: Range Sum Query 2D - Immutable

---
+++ Prefix Sum/0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,35 @@
+class NumMatrix {
+private:
+    vector<vector<int>> prefix;
+
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return;
+        }
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        prefix.assign(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int top = (i > 0) ? prefix[i - 1][j] : 0;
+                int left = (j > 0) ? prefix[i][j - 1] : 0;
+                int topLeft = (i > 0 && j > 0) ? prefix[i - 1][j - 1] : 0;
+
+                prefix[i][j] = matrix[i][j] + top + left - topLeft;
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int total = prefix[row2][col2];
+        int top = (row1 > 0) ? prefix[row1 - 1][col2] : 0;
+        int left = (col1 > 0) ? prefix[row2][col1 - 1] : 0;
+        int topLeft = (row1 > 0 && col1 > 0) ? prefix[row1 - 1][col1 - 1] : 0;
+
+        return total - top - left + topLeft;
+    }
+};
